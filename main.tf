75434052cddfe3f4658c5f43862816f115006566
chaitanya <chaitanyamane14@gmail.com>
2025-10-28 18:55:58 +0530

    deleted old unnecessary files

--- main.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = var.aws_region
-}
-
-resource "aws_instance" "my_ec2" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-}
--- terraform.tfvars
+++
@@ -1,2 +0,0 @@
-aws_region = "ap-northeast-1"
-ami_id     = "ami-0d4aa492f133a3068"
