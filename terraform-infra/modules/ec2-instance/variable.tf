5e556feac61df9cd2622ce4657c1e1b71bc8eaf0
chaitanya <chaitanyamane14@gmail.com>
2025-11-01 13:49:46 +0530

    Restored deleted Terraform files from previous commit

---
+++ terraform-infra/modules/ec2-instance/variable.tf
@@ -0,0 +1,23 @@
+variable "ami" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "instance_name" {
+  type = string
+}
